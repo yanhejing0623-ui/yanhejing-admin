e93593f7613a0b470adf767ed7d20cd26cc726db
yanhejing0623-ui <yan.hejing0623@gmail.com>
2026-06-24 21:20:21 +0800

    Update API_URL to new endpoint

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbwuvsfK1Jr83qaxbctIk2ajgnuO1RoD6S0Xzd4XxO9xPOAO_NMafbUgRYEmpROIA7hxJg/exec";
+"https://script.google.com/macros/s/AKfycbw1Pq3SQ8221OfWhMnrrnB-RVzJB3tm4aQyehiMl0pqrzSjuUw_hPc89EPhHDujY1xeVA/exec";
 
 let adminToken = localStorage.getItem("yh_admin_token") || "";
 let currentUser = null;
